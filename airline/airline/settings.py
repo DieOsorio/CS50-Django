b184e49850b81c623d6306f103b3bae00a2b075c
Diego Osorio <DieOsorio@Outlook.com>
2018-11-27 18:39:35 -0300

    travis

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@5scbb96xh!$c212g06@oy&3et=-j2r6$tu@ve29qa-c@lin!o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
